2ff743ab201991e68c457e75e4da7905f897cc1c
Raul Battistini <raulbattistini.3@gmail.com>
2022-09-29 16:54:34 -0300

    rm: helper file and unnecesary css

--- server/src/helpers/passwordHash.ts
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-interface IHashPassword{ 
-   hashPassword: (password: string) => void
-}
-
-export const hashPassword = (password: string) => bcrypt.hashSync(password, bcrypt.genSaltSync(12));
